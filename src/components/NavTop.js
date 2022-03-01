8f2b38f4ab030e4b27cf6c3e4fac1d80c7ae0e08
Massi Dris <drismassi@hotmail.com>
2022-03-01 12:31:37 +0100

    add notifications styling

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import logo192 from "../img/doc.png";
-import logo193 from "../img/logo192.png";
+import Bell from "../img/Bell.svg";
+import Warning from "../img/warning.svg";
 import { auth, firestore } from "../firebase/firebase.utils";
 
 export default function NavTop({ name }) {
@@ -17,7 +18,6 @@ export default function NavTop({ name }) {
         .collection("notifications")
         .doc(n.id)
         .delete();
-
       return unsubscribe2;
     });
     setNotifications([]);
@@ -47,9 +47,11 @@ export default function NavTop({ name }) {
   return (
     <NavBar className="nav-bar">
       <div className="notification-icon" onClick={() => setOpen(!open)}>
-        <img src={logo193} alt="" />
+        <img src={Bell} alt="" />
         {notifications.length > 0 && (
-          <div className="counter">{notifications.length} </div>
+          <div className="counter-container">
+            <div className="counter">{notifications.length}</div>{" "}
+          </div>
         )}
       </div>
       <div className="text">{user && <h1>{user.displayName}</h1>}</div>
@@ -62,18 +64,20 @@ export default function NavTop({ name }) {
       </div>
       {open && (
         <div className="notifications-menu">
-          {notifications.map((n) => (
-            <>
-              <div key={n.id} className="notification">
-                <img src={logo193} alt="" />
-                <p>{n.msg}</p>
-              </div>
-              <div className="sep-menu"></div>
-            </>
-          ))}
           <button className="nButton" onClick={handleRead}>
             Supprimer les notifications
           </button>
+          <div className="notifictions-content">
+            {notifications.map((n) => (
+              <>
+                <div key={n.id} className="notification">
+                  <img src={Warning} alt="" />
+                  <p>{n.msg}</p>
+                </div>
+                <div className="sep-menu"></div>
+              </>
+            ))}
+          </div>
         </div>
       )}
     </NavBar>
@@ -119,46 +123,66 @@ const NavBar = styled.div`
     img {
       height: 3vh;
       width: 3vh;
+      cursor: pointer;
+    }
+    .counter-container {
+      position: absolute;
+      top: -8px;
+      right: -3px;
+      background: #eb4444;
+      width: 22px;
+      height: 22px;
+      border-radius: 15px;
     }
     .counter {
-      position: absolute;
-      top: -2px;
-      right: -2px;
-      background: red;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      cursor: default;
     }
+  }
+  .notifictions-content {
+    overflow-y: auto;
   }
   .notifications-menu {
     height: 40vh;
     width: 20vw;
+    overflow: hidden;
     display: flex;
     flex-direction: column;
     align-items: center;
-    background: #bac4e0;
+    background: white;
     position: absolute;
     border-radius: 15px;
-    top: 10vh;
+    top: 7vh;
     right: 20vw;
     z-index: 1000;
+    border: 2px solid #243153;
+    margin-bottom: 5px;
     .nButton {
-      width: 80%;
-      padding: 5px;
+      width: 90%;
+      padding: 10px;
       margin-top: 10px;
       background: #243153;
       color: white;
       border: none;
       border-radius: 6px;
+      font-size: 0.9rem;
       cursor: pointer;
     }
   }
   .notification {
-    width: 100%;
     color: black;
     display: flex;
     align-items: center;
     padding: 1rem;
+    margin: 8px;
+    cursor: default;
     &:hover {
-      background: #8fa1ce;
+      background: #bdc7e4;
       border-radius: 10px;
+      transition: 300ms ease;
     }
     img {
       height: 3vh;
